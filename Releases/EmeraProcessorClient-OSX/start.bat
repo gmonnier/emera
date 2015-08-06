94543a7e08a1fac281957eafdb57e32271df87fe
Gilles Monnier <gilles.monnier@searshc.com>
2015-08-06 02:46:19 +0200

    Remove start.bat from os release

--- Releases/EmeraProcessorClient-OSX/start.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-rem --- Dependencies for java VTK Dlls ---
-set JAVA_AWT_BIN_PATH=.\jre7\bin
-
-echo execute java command %JAVA_AWT_BIN_PATH%\java.exe
-%JAVA_AWT_BIN_PATH%\java.exe -jar Application.jar
