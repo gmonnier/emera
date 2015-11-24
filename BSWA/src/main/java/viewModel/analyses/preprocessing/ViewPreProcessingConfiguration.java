7a8c25f28849066c5c73d301892cfcb04bd5f017
Monnier, Gilles <Gilles.Monnier@searshc.com>
2015-11-23 19:38:38 -0800

    Add WS API endpoint for pre processing analyses configuration

---
+++ BSWA/src/main/java/viewModel/analyses/preprocessing/ViewPreProcessingConfiguration.java
@@ -0,0 +1,15 @@
+package viewModel.analyses.preprocessing;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+import logger.Log4JLogger;
+
+import org.apache.logging.log4j.Logger;
+
+@XmlRootElement
+public class ViewPreProcessingConfiguration {
+	
+	// log4j logger - Main logger
+	private static Logger LOG = Log4JLogger.logger;
+
+}
