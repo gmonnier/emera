2513edfec9d468016e954c81132094cf130c746c
Monnier, Gilles <Gilles.Monnier@searshc.com>
2015-11-24 23:06:25 -0800

    Move step 1 javascript into directive

--- BSWA/src/main/webapp/WEB-INF/js/controllers/create-lookup-ctrl.js
+++ BSWA/src/main/webapp/WEB-INF/js/controllers/create-lookup-ctrl.js
@@ -263,4 +263,39 @@ appControllers.controller('createLookupCtrl', function ($scope,$rootScope, $http
 		$('.accrun .ui-state-focus').removeClass('ui-state-focus');
 	}
 
+}).directive('initAccordions', function() {
+	return function(scope, element, attrs) {
+		$('.acc').accordion({
+			heightStyle : "content",
+			autoHeight : false,
+			clearStyle : true,
+			header : "h3",
+			collapsible : true
+		});
+		
+		$("#menuLoadData").menu();
+		$("#menuLoadData").css('position', 'absolute');
+		$("#menuLoadData").css('z-index', '100');
+		$("#menuLoadData").hide();
+
+		$(".open-load-data").hover(function(e) {
+			$("#menuLoadData").width(250);
+			$("#menuLoadData").show();
+		}, function() {
+			$("#menuLoadData").hide();
+			$("#menuLoadData").width(0);
+		});
+		
+		$("#menuLoadLibrary").menu();
+		$("#menuLoadLibrary").css('position', 'absolute');
+		$("#menuLoadLibrary").css('z-index', '100');
+		$("#menuLoadLibrary").hide();
+		$(".open-load-lib").hover(function() {
+			$("#menuLoadLibrary").show();
+			$("#menuLoadLibrary").width(250);
+		}, function() {
+			$("#menuLoadLibrary").hide();
+			$("#menuLoadLibrary").width(0);
+		});
+	};
 });
--- BSWA/src/main/webapp/WEB-INF/js/controllers/create-split-ctrl.js
+++ BSWA/src/main/webapp/WEB-INF/js/controllers/create-split-ctrl.js
@@ -26,5 +26,30 @@ appControllers.controller('createSplitCtrl', function($scope, $http) {
 			header : "h3",
 			collapsible : true
 		});
+		
+		$("#menuLoadData").menu();
+		$("#menuLoadData").css('position', 'absolute');
+		$("#menuLoadData").css('z-index', '100');
+		$("#menuLoadData").hide();
+
+		$(".open-load-data").hover(function(e) {
+			$("#menuLoadData").width(250);
+			$("#menuLoadData").show();
+		}, function() {
+			$("#menuLoadData").hide();
+			$("#menuLoadData").width(0);
+		});
+		
+		$("#menuLoadLibrary").menu();
+		$("#menuLoadLibrary").css('position', 'absolute');
+		$("#menuLoadLibrary").css('z-index', '100');
+		$("#menuLoadLibrary").hide();
+		$(".open-load-lib").hover(function() {
+			$("#menuLoadLibrary").show();
+			$("#menuLoadLibrary").width(250);
+		}, function() {
+			$("#menuLoadLibrary").hide();
+			$("#menuLoadLibrary").width(0);
+		});
 	};
 });
