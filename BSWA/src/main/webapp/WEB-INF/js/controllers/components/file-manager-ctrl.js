e77abed27c41e920a0089e22d343f513d89a0098
Monnier, Gilles <Gilles.Monnier@searshc.com>
2015-11-28 20:48:44 -0800

    Ensure uniqueness of file loading components

--- BSWA/src/main/webapp/WEB-INF/js/controllers/components/file-manager-ctrl.js
+++ BSWA/src/main/webapp/WEB-INF/js/controllers/components/file-manager-ctrl.js
@@ -1,7 +1,56 @@
-app.controller('fileManagerController', function() {
+app.controller('fileManagerController', function(uploadService) {
+
 	this.title = 'Generic file manager';
+	this.uid = 'generic';
 	this.storedFilesList = [];
 	this.selectedFiles = [];
+	this.fileUploader = uploadService.getDataUploader();
+	this.baseSpaceAllowed = true;
+
+	this.openBaseSpaceSelectionDial = function() {
+		// Open dialog
+		$("#basespacedialog").dialog({
+			modal : true,
+			width : 700,
+			show : {
+				effect : 'fade',
+				duration : 1000
+			},
+			hide : {
+				effect : 'fade',
+				duration : 1000
+			},
+			close : $(".accrun").accordion({
+				heightStyle : "content",
+				autoHeight : false,
+				clearStyle : true,
+				active : false,
+				header : "h3",
+				collapsible : true
+			}),
+			buttons : {
+				Ok : function() {
+					// emit event to ask bs-model-ctrl to add selected file to
+					// current model
+					$rootScope.$emit('addSelectedFile');
+					$(this).dialog("close");
+					$scope.$apply();
+				}
+			}
+		});
+		// clear selection and close accordions
+		$('.accrun .ui-state-focus').removeClass('ui-state-focus');
+	};
+
+	this.selectFile = function(elemId) {
+		var elem = document.getElementById(elemId);
+		if (elem && document.createEvent) {
+			var evt = document.createEvent("MouseEvents");
+			evt.initEvent("click", true, false);
+			elem.dispatchEvent(evt);
+		}
+	}
+
 }).directive('initAccordions', function() {
 	return function(scope, element, attrs) {
 		$('.acc').accordion({
