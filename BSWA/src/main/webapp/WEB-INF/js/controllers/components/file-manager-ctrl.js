82c47de130cb99ccebd0a5799dea701ae0c5e170
Monnier, Gilles <Gilles.Monnier@searshc.com>
2015-11-29 14:01:52 -0800

    Bind callbacks functions to this

--- BSWA/src/main/webapp/WEB-INF/js/controllers/components/file-manager-ctrl.js
+++ BSWA/src/main/webapp/WEB-INF/js/controllers/components/file-manager-ctrl.js
@@ -42,7 +42,41 @@ app.controller('fileManagerController', function(uploadService) {
 		// clear selection and close accordions
 		$('.accrun .ui-state-focus').removeClass('ui-state-focus');
 	};
+	
+	this.addViewFile = angular.bind(this, function(file) {
+		if(file !== null) {
+			var ok = this.checkDuplicates(this.selectedFiles,file);
+			if(ok) {
+				this.selectedFiles.push(file);
+				return true;
+			} else {
+				var dialogSelector = "#dialog-duplicate-warning-" + this.uid;
+				this.showDialog(dialogSelector);
+				return false;
+			}
+		}
+	}
+	);
+	
+	this.fileUploader.onAfterAddingFile = angular.bind(this, function(fileItem) { 
+		// TODO Remove reference to upload DATA (too specific)
+		var success = uploadService.addToUploadData(fileItem, this.addViewFile); 
+		if(!success){
+			this.fileUploader.removeFromQueue(fileItem);
+		}
+		}
+	);
 
+	this.checkDuplicates = function(filelist, file) {
+		var arrayLength = filelist.length;
+		for (var i = 0; i < arrayLength; i++) {
+			if(filelist[i].id === file.id) {
+				return false;
+			}
+		}
+		return true;
+	};
+	
 	this.selectFile = function() {
 		var elem = document.getElementById(this.localFileInputSelector);
 		if (elem && document.createEvent) {
@@ -57,8 +91,32 @@ app.controller('fileManagerController', function(uploadService) {
 			return list.length == 0;
 		}
 		return true;
-	}
+	};
 
+	
+	this.showDialog = function(dialogid, onok) {
+		$(dialogid).dialog({
+			modal: true,
+			width: 700,
+			show: {
+		        effect: 'fade',
+		        duration: 1000
+		    },
+		    hide: {
+		        effect: 'fade',
+		        duration: 1000
+		    },
+			buttons: {
+			Ok: function() {
+				$( this ).dialog( "close" );
+				if(onok) {
+					onok();
+				}
+			}
+			}
+		});
+	};
+	
 }).directive('initAccordions', function() {
 	return function(scope, element, attrs) {
 		$('.acc').accordion({
--- BSWA/src/main/webapp/WEB-INF/js/directives/file-manager-directive.js
+++ BSWA/src/main/webapp/WEB-INF/js/directives/file-manager-directive.js
@@ -1,7 +1,9 @@
 app.directive('fileManager', function($rootScope) {
 	return {
+		scope: {},
 		templateUrl : '/templates/file-manager-template.html',
 	    controller: 'fileManagerController',
 	    controllerAs: 'ctrl',
+	    bindToController: true
 	};
 });
