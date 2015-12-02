f4ca2d01be5a95adb15e0b2071ecc8babe2a99a9
Monnier, Gilles <Gilles.Monnier@searshc.com>
2015-12-01 17:03:37 -0800

    Apply jQuery-init directive where necessary

--- BSWA/src/main/webapp/WEB-INF/js/controllers/create-split-ctrl.js
+++ BSWA/src/main/webapp/WEB-INF/js/controllers/create-split-ctrl.js
@@ -1,20 +1,22 @@
 appControllers.controller('createSplitCtrl', function($scope, $http) {
 
-	$http({method: 'GET', url: '/ws-resources/datastorage/dataFiles'}).
-    success(function(data, status, headers, config) {
-        $scope.storeDataFilesList = data;
-    }).
-    error(function(data, status, headers, config) {
+	$http({
+		method : 'GET',
+		url : '/ws-resources/datastorage/dataFiles'
+	}).success(function(data, status, headers, config) {
+		$scope.storeDataFilesList = data;
+	}).error(function(data, status, headers, config) {
 		$scope.storeDataFilesList = [];
-    });
-	
+	});
+
 	// Retrieve init processing configuration
-	$http({method: 'GET', url: '/ws-resources/preprocess/confinit'}).
-    success(function(data, status, headers, config) {
+	$http({
+		method : 'GET',
+		url : '/ws-resources/preprocess/confinit'
+	}).success(function(data, status, headers, config) {
 		$scope.configuration = data;
-    }).
-    error(function(data, status, headers, config) {
+	}).error(function(data, status, headers, config) {
 		$scope.configuration = null;
-    });
-	
+	});
+
 });
