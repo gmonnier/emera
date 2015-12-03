2b03bd774e6f730eb93298b7464989d74d92f963
Monnier, Gilles <Gilles.Monnier@searshc.com>
2015-12-02 18:35:55 -0800

    Adjust backend model and endpoint to handle input data selection

--- BSWA/src/main/java/viewModel/analyses/preprocessing/ViewPreProcessingConfiguration.java
+++ BSWA/src/main/java/viewModel/analyses/preprocessing/ViewPreProcessingConfiguration.java
@@ -1,15 +1,41 @@
 package viewModel.analyses.preprocessing;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.xml.bind.annotation.XmlRootElement;
 
 import logger.Log4JLogger;
 
 import org.apache.logging.log4j.Logger;
 
+import viewModel.ViewFile;
+
 @XmlRootElement
 public class ViewPreProcessingConfiguration {
-	
+
 	// log4j logger - Main logger
 	private static Logger LOG = Log4JLogger.logger;
 
+	// List of data files
+	private List<ViewFile> selectedDataFiles;
+
+	public ViewPreProcessingConfiguration() {
+
+		selectedDataFiles = new ArrayList<ViewFile>();
+
+	}
+
+	public List<ViewFile> getSelectedDataFiles() {
+		return selectedDataFiles;
+	}
+
+	public void setSelectedDataFiles(List<ViewFile> selectedDataFiles) {
+		this.selectedDataFiles = selectedDataFiles;
+	}
+
+	@Override
+	public String toString() {
+		return "ViewPreProcessingConfiguration [selectedDataFiles=" + selectedDataFiles + "]";
+	}
 }
--- BSWA/src/main/webapp/WEB-INF/js/controllers/create-split-ctrl.js
+++ BSWA/src/main/webapp/WEB-INF/js/controllers/create-split-ctrl.js
@@ -21,5 +21,78 @@ appControllers.controller('createSplitCtrl', function($scope, $http, uploadServi
 	}).error(function(data, status, headers, config) {
 		$scope.configuration = null;
 	});
+	
+    $scope.$on('nextStepClicked', function (evt, directiveScope) {
+    	if((directiveScope.currentCreateStep+1) > directiveScope.MAX_STEP) {
+			if($scope.validateFileSelection()) {
+				alert("start new split analysis");
+				console.log($scope.configuration);
+				//$scope.enqueueProcessing();
+			} else {
+				$scope.showDialog("#dialog-unvalid-parameters");
+			}
+		}
+    });
+    
+    $scope.enqueueProcessing = function() {
+		var userID = 'guest';
+		if(user.current.hasOwnProperty('user_id')) {
+			userID = user.current.user_id;
+		}
+		var urlenqueue = '/ws-resources/preprocess/enqueue?user_id=' + userID;
+		
+		var request = $http({method: 'POST', 
+							url: urlenqueue,
+							data: $scope.configuration});
+		
+		request.success(function(data, status, headers, config) {
+			var uploadNeeded = uploadService.uploadNeeded();
+			$scope.uploadmessage = uploadNeeded ? 'Upload process will start now. Please do not leave the page while transfering files to server.' : '';
+			if(uploadNeeded && data) {
+				// ask the upload service to start uploading files to the server - data = analyse ID
+				uploadService.startUpload(data);
+			} else {
+				if(!data) {
+					// should never occurs!
+					alert('an error occured when uploading files, unable to retrieve analyse id');
+				}
+			}
+			$scope.showDialog("#dialog-message-ok", function() {
+				$scope.changeRoute('/running');
+			});
+		}).
+		error(function(data, status, headers, config) {
+			$scope.showDialog("#dialog-message-failed" );
+			$scope.changeRoute('/home');
+		});
+	}
+    
+	$scope.validateFileSelection = function() {
+		return $scope.configuration.selectedDataFiles.length > 0;
+	}
+    
+    // TODO Refactor this method
+	$scope.showDialog = function(dialogid, onok) {
+		$(dialogid).dialog({
+			modal: true,
+			width: 700,
+			show: {
+		        effect: 'fade',
+		        duration: 500
+		    },
+		    hide: {
+		        effect: 'fade',
+		        duration: 500
+		    },
+			buttons: {
+			Ok: function() {
+				$( this ).dialog( "close" );
+				if(onok) {
+					onok();
+				}
+			}
+			}
+		});
+	}
 
 });
