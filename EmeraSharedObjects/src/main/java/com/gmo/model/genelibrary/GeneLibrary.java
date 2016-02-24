15bab561dd8b09ecea713d7608242e0ffed05295
Monnier, Gilles <Gilles.Monnier@searshc.com>
2016-02-23 21:55:15 -0800

    Default jdk to 1.8 for shared model maven project

---
+++ EmeraSharedObjects/src/main/java/com/gmo/model/genelibrary/GeneLibrary.java
@@ -0,0 +1,68 @@
+package com.gmo.model.genelibrary;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlTransient;
+
+import com.gmo.util.StringSerializable;
+import com.gmo.util.StringSerializationException;
+
+public class GeneLibrary implements Serializable, StringSerializable {
+
+	@XmlTransient
+	private List<ReferenceGene> genes;
+
+	private static final StringBuffer sb = new StringBuffer();
+
+	public GeneLibrary() {
+		genes = new ArrayList<>();
+	}
+
+	public void add(ReferenceGene newGene) {
+		genes.add(newGene);
+	}
+
+	@XmlTransient
+	public List<ReferenceGene> getGenes() {
+		return genes;
+	}
+
+	@XmlTransient
+	public void setGenes(List<ReferenceGene> genes) {
+		this.genes = genes;
+	}
+
+	public int size() {
+		return genes.size();
+	}
+
+	@Override
+	public String toString() {
+		return "GeneLibrary [genesIds size=" + genes.size() + "]";
+	}
+
+	@Override
+	public String getObjectAsString() {
+		sb.setLength(0);
+		for (int i = 0; i < genes.size(); i++) {
+			sb.append(genes.get(i).getObjectAsString());
+			if (i != genes.size() - 1) {
+				sb.append("##");
+			}
+		}
+		return sb.toString();
+	}
+
+	@Override
+	public void convertStringToObject(String input) throws StringSerializationException {
+		String[] splitted = input.split("##");
+		for (int i = 0; i < splitted.length; i++) {
+			ReferenceGene gene = new ReferenceGene("","");
+			gene.convertStringToObject(splitted[i]);
+			genes.add(gene);
+		}
+	}
+
+}
---
+++ EmeraSharedObjects/src/main/java/com/gmo/model/parameters/PartialProcessConfiguration.java
@@ -0,0 +1,81 @@
+package com.gmo.model.parameters;
+
+import java.io.Serializable;
+
+import com.gmo.util.StringSerializable;
+import com.gmo.util.StringSerializationException;
+
+public class PartialProcessConfiguration implements Serializable, StringSerializable {
+
+	// The selected extraction pattern
+	protected ExtractionPattern pattern;
+
+	// Pattern attributes
+	protected PatternAttributes patternAttributes;
+
+	// Output configuration
+	protected OutputAttributes outputAttributes;
+
+	private StringBuilder sb = new StringBuilder();
+
+	public PartialProcessConfiguration() {
+
+	}
+
+	public ExtractionPattern getPattern() {
+		return pattern;
+	}
+
+	public void setPattern(ExtractionPattern pattern) {
+		this.pattern = pattern;
+	}
+
+	public PatternAttributes getPatternAttributes() {
+		return patternAttributes;
+	}
+
+	public void setPatternAttributes(PatternAttributes patternAttributes) {
+		this.patternAttributes = patternAttributes;
+	}
+
+	public OutputAttributes getOutputAttributes() {
+		return outputAttributes;
+	}
+
+	public void setOutputAttributes(OutputAttributes outputAttributes) {
+		this.outputAttributes = outputAttributes;
+	}
+
+	@Override
+	public String toString() {
+		return "ProcessConfiguration -----> \n pattern=" + pattern + "\npatternAttributes=" + patternAttributes + "\noutputAttributes=" + outputAttributes + "]";
+	}
+
+	@Override
+	public String getObjectAsString() {
+		sb.setLength(0);
+		sb.append(pattern.getObjectAsString());
+		sb.append("##");
+		sb.append(patternAttributes.getObjectAsString());
+		sb.append("##");
+		sb.append(outputAttributes.getObjectAsString());
+		return sb.toString();
+	}
+
+	@Override
+	public void convertStringToObject(String input) throws StringSerializationException {
+		String[] splitted = input.split("##");
+		if (splitted == null || splitted.length != 3) {
+			throw new StringSerializationException();
+		} else {
+			this.pattern = new ExtractionPattern();
+			this.pattern.convertStringToObject(splitted[0]);
+
+			this.patternAttributes = new PatternAttributes();
+			this.patternAttributes.convertStringToObject(splitted[1]);
+
+			this.outputAttributes = new OutputAttributes();
+			this.outputAttributes.convertStringToObject(splitted[2]);
+		}
+	}
+}
