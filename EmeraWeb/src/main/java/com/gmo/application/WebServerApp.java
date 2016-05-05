3b3ffb550905bed7806ade2ec46522a1f200005b
Gilles monnier <gilles_monnier@yahoo.fr>
2016-05-05 14:18:55 -0700

    Review S3 Extractor to significantly reduce the amount of requests

--- EmeraWeb/src/main/java/com/gmo/application/WebServerApp.java
+++ EmeraWeb/src/main/java/com/gmo/application/WebServerApp.java
@@ -20,7 +20,6 @@ import com.gmo.application.connectionsMonitor.ConnectionsMonitor;
 import com.gmo.application.filters.ConnectionFilter;
 import com.gmo.application.mappers.EOFExceptionMapper;
 import com.gmo.application.mappers.WebExceptionMapper;
-import com.gmo.basespaceService.interfaces.IBaseSpaceModel;
 import com.gmo.commonconfiguration.NetworkTopologyManager;
 import com.gmo.configuration.ApplicationContextManager;
 import com.gmo.externalinterfaces.nodes.rmiserver.NodeNotificationsRMIServer;
--- EmeraWeb/src/main/java/com/gmo/results/extractor/AnalysisS3Extractor.java
+++ EmeraWeb/src/main/java/com/gmo/results/extractor/AnalysisS3Extractor.java
@@ -1,11 +1,12 @@
 package com.gmo.results.extractor;
 
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.logging.log4j.Logger;
-
-import awsinterfaceManager.AWSS3InterfaceManager;
 
 import com.gmo.configuration.ApplicationContextManager;
 import com.gmo.logger.Log4JLogger;
@@ -15,6 +16,8 @@ import com.gmo.processorNode.viewmodel.analyses.standard.ViewAnalysis;
 import com.gmo.results.ResultsManager;
 import com.gmo.sharedobjects.model.analysis.AnalysisStatus;
 import com.gmo.sharedobjects.model.reports.Report;
+
+import awsinterfaceManager.AWSS3InterfaceManager;
 
 public class AnalysisS3Extractor extends AnalysisExtractor {
 
@@ -33,83 +36,91 @@ public class AnalysisS3Extractor extends AnalysisExtractor {
 	@Override
 	protected void extractAnalyses() {
 
-		List<String> userRepositories = AWSS3InterfaceManager.getInstance().listUsersRepositories(analysesDirectoryRoot);
-		if (userRepositories.isEmpty()) {
-			LOG.warn("No users results repository, exits extractor thread.");
-			return;
-		}
+		List<Object[]> bucketContent = AWSS3InterfaceManager.getInstance().listAllFilesInBucket(analysesDirectoryRoot);
 
-		for (int k = 0; k < userRepositories.size(); k++) {
+		Map<String, AnalysisS3Model> extractedAnalyses = new HashMap<>();
+		for (int k = 0; k < bucketContent.size(); k++) {
+			Object[] fileInfo = bucketContent.get(k);
+			String filePath = (String) fileInfo[0];
+			String[] decomposedPath = filePath.split("/");
 
-			String userIDPrefix = userRepositories.get(k);
-			String userID = userIDPrefix;
-			if(userID.endsWith("/")) {
-				// Remove remaining directory separator
-				userID = userID.substring(0, userID.length()-1);
-			}
-			List<String> listAnalysisDir = AWSS3InterfaceManager.getInstance().listDirectories(analysesDirectoryRoot, userIDPrefix);
+			try {
+				String filename = decomposedPath[decomposedPath.length - 1];
+				String userID = decomposedPath[0];
+				String analysisID = decomposedPath[1];
 
-			if (listAnalysisDir.isEmpty()) {
-				LOG.warn("Analyses results directory for user " + userID + " is empty, exits extractor thread.");
-			}
-
-			LOG.warn("Extracting analyses for user " + userID);
-
-			for (int i = 0; i < listAnalysisDir.size(); i++) {
-
-				String analysePath = listAnalysisDir.get(i);
-				LOG.debug("\tExtracting analysis " + i + "/" + listAnalysisDir.size() + "  " + analysePath);
-
-				String[] path = analysePath.split("/");
-				String analyseID = path[path.length - 1];
-
-				List<Object[]> listFiles = AWSS3InterfaceManager.getInstance().listFiles(analysesDirectoryRoot, analysePath);
-
-				String reportSerialized = analysePath + REPORT_FILENAME;
-				InputStream reportStream = AWSS3InterfaceManager.getInstance().getObjectInputStream(analysesDirectoryRoot, reportSerialized);
-
-				try {
-
-					Report report = ReportReader.extractReport(reportStream, userID);
-
-					ViewAnalysis analysisDone = new ViewAnalysis();
-					analysisDone.setId(analyseID);
-					analysisDone.setUserid(userID);
-					analysisDone.setStatus(AnalysisStatus.DONE);
-					analysisDone.setLaunchDate(report.getStartDate());
-					analysisDone.setCompletionDate(report.getEndDate());
-					analysisDone.setReport(report);
-					analysisDone.setViewConfiguration(report.getAnalyseConfig());
-
-					extractAdditionnalAnalyses(analysisDone, analysePath);
-
-					LOG.warn("Analysis extraction done for " + analyseID + " assossiated with user : " + userID + ". Add to processed analysis list");
-					ResultsManager.getInstance().addProcessedAnalysis(analysisDone);
-
-				} catch (Throwable ex) {
-					LOG.error("Unable to deserialize Report file for analysis id " + analyseID, ex);
+				AnalysisS3Model analysisModel = extractedAnalyses.get(analysisID);
+				if (analysisModel == null) {
+					analysisModel = new AnalysisS3Model();
+					extractedAnalyses.put(analysisID, analysisModel);
 				}
 
+				// Extract main report
+				if (filename.equalsIgnoreCase(REPORT_FILENAME)) {
+					analysisModel.extractReport(filePath, userID, analysisID);
+				} else if (decomposedPath.length > 3 && decomposedPath[2].equals(ADDITIONAL_ANALYSIS_DIR)) {
+					analysisModel.addAdditionnalAnalysisResultFile(fileInfo, filename);
+				}
+				// No need to extract csv and pdf files. Paths are generated
+				// from the View configuration
+			} catch (ArrayIndexOutOfBoundsException aio) {
+				LOG.error("Extracted key is not valid for results extraction : " + fileInfo[0]);
 			}
 		}
+
+		Iterator<Map.Entry<String, AnalysisS3Model>> it = extractedAnalyses.entrySet().iterator();
+		while (it.hasNext()) {
+			Map.Entry<String, AnalysisS3Model> pair = (Map.Entry<String, AnalysisS3Model>) it.next();
+			pair.getValue().addToProcessedAnalyses();
+		}
+
 		LOG.warn("Analysis extraction done");
 	}
 
-	private void extractAdditionnalAnalyses(ViewAnalysis analysisDone, String analysisPath) {
+	class AnalysisS3Model {
 
-		String additionnalPath = analysisPath + ADDITIONAL_ANALYSIS_DIR + "/";
+		private ViewAnalysis analysisDone;
 
-		List<Object[]> listAdditionnalFiles = AWSS3InterfaceManager.getInstance().listFiles(analysesDirectoryRoot, additionnalPath);
+		private boolean isAnalysisValid;
 
-		if (listAdditionnalFiles.isEmpty()) {
-			LOG.debug("No additional analyses found for " + analysisDone.getId() + " in " + additionnalPath);
-		} else {
-			LOG.debug(listAdditionnalFiles.size() + " additional analyses found for " + analysisDone.getId() + " in " + additionnalPath);
-			for (int i = 0; i < listAdditionnalFiles.size(); i++) {
-				Object[] fileData = listAdditionnalFiles.get(i);
-				String[] filePath = fileData[0].toString().split("/");
-				String name = filePath[filePath.length - 1];
-				analysisDone.getAdditionalAnalyses().add(new ViewFile(ViewFileOrigin.S3, name, fileData[0].toString(), (long) fileData[2], (long) fileData[1]));
+		public AnalysisS3Model() {
+			isAnalysisValid = false;
+			this.analysisDone = new ViewAnalysis();
+		}
+
+		public void addAdditionnalAnalysisResultFile(Object[] fileData, String fileName) {
+			analysisDone.getAdditionalAnalyses().add(new ViewFile(ViewFileOrigin.S3, fileName, fileData[0].toString(), (long) fileData[2], (long) fileData[1]));
+		}
+
+		public void extractReport(String pathToReport, String userID, String analyseID) {
+
+			InputStream reportStream = AWSS3InterfaceManager.getInstance().getObjectInputStream(analysesDirectoryRoot, pathToReport);
+
+			try {
+
+				Report report = ReportReader.extractReport(reportStream, userID);
+
+				analysisDone.setId(analyseID);
+				analysisDone.setUserid(userID);
+				analysisDone.setStatus(AnalysisStatus.DONE);
+				analysisDone.setLaunchDate(report.getStartDate());
+				analysisDone.setCompletionDate(report.getEndDate());
+				analysisDone.setReport(report);
+				analysisDone.setViewConfiguration(report.getAnalyseConfig());
+
+			} catch (Throwable ex) {
+				LOG.error("Unable to deserialize Report file for analysis id " + analyseID, ex);
+			}
+
+			isAnalysisValid = true;
+		}
+
+		public void addToProcessedAnalyses() {
+			if (isAnalysisValid) {
+				LOG.info("Analysis extraction done for " + analysisDone.getId() + " assossiated with user : " + analysisDone.getUserid() + ". Add to processed analysis list");
+				ResultsManager.getInstance().addProcessedAnalysis(analysisDone);
+			} else {
+				LOG.error("Extracted analysis is not valid - check whether all appropriates files are presents " + analysisDone.getId());
 			}
 		}
 	}
