88f98adf7294a17f3e6d32ceddeed6575c535875
Gilles monnier <gilles_monnier@yahoo.fr>
2016-05-10 18:51:03 -0700

    Processor Node interface improvements

--- RMICommonModel/src/main/java/com/gmo/processorNode/interfaces/IProcessorNotifications.java
+++ RMICommonModel/src/main/java/com/gmo/processorNode/interfaces/IProcessorNotifications.java
@@ -3,10 +3,18 @@ package com.gmo.processorNode.interfaces;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
+import com.gmo.generated.configuration.applicationcontext.ResultLocation;
+import com.gmo.processorNode.viewmodel.ViewFile;
 import com.gmo.processorNode.viewmodel.analyses.standard.ViewAnalysis;
 
 public interface IProcessorNotifications extends Remote {
 
 	public void analysisCompleted(ViewAnalysis analysis) throws RemoteException;
+
+	public void additionnalAnalysisCompleted(String analysisID, ViewFile outputPDF) throws RemoteException;
+
+	public void additionnalAnalysisFailed(String analysisID, String reasonMessage) throws RemoteException;
+
+	public ResultLocation fetchResultsLocation() throws RemoteException;
 
 }
