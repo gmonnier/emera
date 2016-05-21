2ee11f4a6f17ef9f3d59946fe66482c7b51c7adb
Gilles monnier <gilles_monnier@yahoo.fr>
2016-05-21 13:34:23 -0700

    Css tasks within gulp pipeline

--- EmeraWeb/gulpfile.js
+++ EmeraWeb/gulpfile.js
@@ -8,16 +8,31 @@ function handleError(err) {
   console.log(err.toString());
 }
 
-gulp.task('clean', function () {
+gulp.task('js-clean', function () {
     return gulp.src('src/main/webapp/resources/javascript/**/*-compiled.js', {read: false})
     .pipe(clean());
 });
 
-gulp.task('js', ['clean'], function () {
+gulp.task('css-clean', function () {
+    return gulp.src('src/main/webapp/resources/javascript/**/*-compiled.css', {read: false})
+    .pipe(clean());
+});
+
+gulp.task('js', ['js-clean'], function () {
   gulp.src(['src/main/webapp/resources/javascript/**/angular-app-init.js', 'src/main/webapp/resources/javascript/**/*.js'])
     .pipe(concat('src/main/webapp/resources/javascript/angular-app-compiled.js'))
     .pipe(ngAnnotate())
     .pipe(uglify())
+    .pipe(gulp.dest('.'))
+    .on('error', handleError);
+})
+
+var cleanCSS = require('gulp-clean-css');
+var concatCss = require('gulp-concat-css');
+gulp.task('css', ['css-clean'], function () {
+  gulp.src(['src/main/webapp/resources/css/**/*.css'])
+    .pipe(cleanCSS())
+    .pipe(concatCss("src/main/webapp/resources/css/emera-compiled.css"))
     .pipe(gulp.dest('.'))
     .on('error', handleError);
 })
